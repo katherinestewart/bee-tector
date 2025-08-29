0d17e5adc938ac202e876b624258c942048ad104
Katherine Stewart <188507117+katherinestewart@users.noreply.github.com>
2025-08-29 23:27:35 +0100

    chore(config): add dataset paths and training parameters

---
+++ config.py
@@ -0,0 +1,8 @@
+# Paths
+FULL_DATA_DIR = "raw_data/bombus12_full"
+CURATED_DATA_DIR = "raw_data/bombus11_curated"
+
+# Parameters
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 32  # Adjust based on your memory capacity
+SEED = 42
