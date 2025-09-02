9a40187717f07af359974cc98bea5d812c0724ec
Katherine Stewart <188507117+katherinestewart@users.noreply.github.com>
2025-09-02 14:43:02 +0100

    added best model path

--- bee_tector/config.py
+++ bee_tector/config.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 FULL_DATA_DIR = PROJECT_ROOT / "raw_data" / "bombus12_full"
 CURATED_DATA_DIR = PROJECT_ROOT / "raw_data" / "bombus11_curated"
 MODELS_DIR = PROJECT_ROOT / "models"
+BEST_MODEL_PATH = PROJECT_ROOT / "models" / "baseline_model"
 
 # Parameters
 IMAGE_SIZE = (224, 224)
