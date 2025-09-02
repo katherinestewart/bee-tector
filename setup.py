f1911746728536063a179f781dd95cfae0d5c0cb
Emanuele Torrisi <veegreb@gmail.com>
2025-09-02 12:31:12 +0200

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name="BeeTector",
+    version="0.1.0",
+    description="BeeTector ML model and API",
+    license="MIT",
+    author="Katherine Stewart",
+    author_email="katherine.stewart1@gmail.com",
+    install_requires=requirements,
+    packages=find_packages(),
+    test_suite="tests",
+    include_package_data=True,
+)
