64bcda62fe933df2ed7b3504981798773a9b86e9
Katherine Stewart <188507117+katherinestewart@users.noreply.github.com>
2025-09-08 21:40:24 +0100

    Edited config with CSV paths

--- bee_tector/config.py
+++ bee_tector/config.py
@@ -9,7 +9,8 @@ CURATED_DATA_DIR = PROJECT_ROOT / "raw_data" / "bombus11_curated"
 MODELS_DIR = PROJECT_ROOT / "models"
 BEST_MODEL_PATH = PROJECT_ROOT / "models" / "inc_base.keras"
 BEES_CSV_PATH = PROJECT_ROOT / "raw_data" / "bombus_inaturalist_cc.csv"
-BEES_COUNTRIES_CSV = PROJECT_ROOT / "raw_data" / "bees_with_countries.csv"
+BEES_COUNTRIES_CSV = PROJECT_ROOT / "raw_data" / "bees_with_countries_raw.csv"
+COUNTRIES_PREDICT = PROJECT_ROOT / "raw_data" / "bees_with_countries.csv"
 
 # Parameters
 IMAGE_SIZE = (299, 299)
