15bcbb5b122d2a24bd06db7dba49b41136a27505
Emanuele Torrisi <veegreb@gmail.com>
2025-09-09 14:44:51 +0200

    ready to use

--- api/fast.py
+++ api/fast.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Form
 import shutil
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from api.predict import preprocess_image, load_best_model, predict
+from bee_tector.pipeline import run_pipeline
 
 app = FastAPI()
 
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 @app.post("/upload_image/")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file(files: UploadFile = File(...), data: str = Form(None)):
     # Check if file is an image
     # if not file.content_type.startswith("image/"):
     #     raise HTTPException(status_code=400, detail="File must be an image")
@@ -28,21 +28,18 @@ async def create_upload_file(file: UploadFile = File(...)):
     """
     # file path to save the uploaded image
     # print(file)
-    file_path = f"uploaded_{file.filename}"
+    file_path = f"uploaded_{files.filename}"
     # print(file_path)
-    # import ipdb; ipdb.set_trace()    
+    # import ipdb; ipdb.set_trace()
     with open(file_path, "wb") as test_img:
         # saving img to file test_img
-        shutil.copyfileobj(file.file, test_img)
-            
+        shutil.copyfileobj(files.file, test_img)
+    print("test")
     # return {"filename": file.filename, "message": "Image uploaded successfully!"}
-    prediction = get_predict(file_path) # dict
+    prediction = run_pipeline(file_path, country_code = data) # dict
     return prediction
 
-def get_predict(img_path):
-    img_array = preprocess_image(img_path)
-    model = load_best_model()
-    return predict(model, img_array)
+
 
 @app.get("/")
 def root():
